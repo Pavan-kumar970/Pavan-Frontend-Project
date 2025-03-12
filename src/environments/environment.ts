565a6ba7f63c45d0eb1ef018d574f77e07feec7c
Pavan-kumar970 <138006253+Pavan-kumar970@users.noreply.github.com>
2025-03-12 21:44:30 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://pavan-backend-project.up.railway.app' // ✅ Your backend URL
+  apiUrl: 'https://pavan-backend-project.up.railway.app/api' // ✅ Your backend URL
 };
